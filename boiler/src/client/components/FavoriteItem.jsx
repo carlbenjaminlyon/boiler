2fab91c5091d3ba133a3c8043ea2ee4cbadb3d19
Carl Benjamin Lyon <carlbenjaminlyon@gmail.com>
2021-11-22 09:19:46 -0600

    building out front end structure

---
+++ boiler/src/client/components/FavoriteItem.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const FavoriteItem = () => {
+  const [item] = useState(null);
+
+  return (
+    <div>
+      <h3>Favorite Item</h3>
+    </div>
+  );
+
+};
+
+export default FavoriteItem;
