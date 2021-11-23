10221b28dbd90064e78a8a40c9d1936d255d0623
Carl Benjamin Lyon <carlbenjaminlyon@gmail.com>
2021-11-23 12:25:13 -0600

    weather component rendering with dynamic icons

--- boiler/src/client/components/Weather.jsx
+++ boiler/src/client/components/Weather.jsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+// import ACCUWEATHER from process.env;
 // import axios from 'axios';
 // import { TextField } from '@mui/material/TextField';
 // import { Autocomplete } from '@mui/material/Autocomplete';
@@ -18,10 +19,27 @@ const Weather = () => {
 
 
 
-  const apiKey = `DcCqa6AACslGAnHwyHTSxi1KqS5D8CG2`;
+  const apiKey = `JzqsG2M1k9uBvVQigY12bAs0PNltgrkr`;
+  // const apiKey = ACCUWEATHER;
+
   const locationKey = `30307_PC`;
 
   const [weatherInfo, setWeatherInfo] = useState();
+
+  //api expects two digit number to represent icon filename
+  const padNum = (num) => {
+    const stringNum = num + '';
+    console.log(typeof stringNum);
+    const stringLength = stringNum.length;
+
+    if (stringLength === 1) {
+      return '0' + stringNum; // 1 --> 01
+    } else {
+      return stringNum; // 12 --> 12
+    }
+  };
+
+
   //only gets called when component mounts
   useEffect(() => {
     fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey=${locationKey}?apikey=${apiKey}`)
@@ -31,21 +49,22 @@ const Weather = () => {
           min: daily.Temperature.Minimum.Value,
           max: daily.Temperature.Maximum.Value,
           weatherType: daily.Day.IconPhrase,
+          weatherIcon: daily.Day.Icon,
+          // weatherIcon: padNum(daily.Day.Icon)
         };
       })));
   }, []);
-
-  //testing to see if it returns how I want it to return
-  // useEffect(() => {
-  //   console.log(weatherInfo);
-  // }, [weatherInfo]);
 
 
   return (
     <div>
       {!!weatherInfo && weatherInfo.map((i, index) => (
         <div key={index}>
-          <WeatherDay min={i.min} max={i.max} weatherType={i.weatherType} />
+          <WeatherDay
+            min={i.min}
+            max={i.max}
+            weatherType={i.weatherType}
+            weatherIcon={i.weatherIcon} />
         </div>
       ))}
     </div>
--- boiler/src/client/components/WeatherDay.jsx
+++ boiler/src/client/components/WeatherDay.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 
 
-const WeatherDay = ({min, max, weatherType}) => {
-
+const WeatherDay = ({min, max, weatherType, weatherIcon}) => {
+  console.log(weatherIcon);
   return (
     <div>
+      <div>Icon:
+        <img src={`https://developer.accuweather.com/sites/default/files/0${weatherIcon}-s.png`} />
+      </div>
       <div>Type: {weatherType}</div>
       <div>Temp: {min} / {max}</div>
     </div>
