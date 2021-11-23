90f4eab08502de87f2b6d4705754fcae449a5705
Amelia Neville <muzikgirl12@gmail.com>
2021-11-23 12:28:44 -0600

    search map feature

--- boiler/src/server/index.js
+++ boiler/src/server/index.js
@@ -21,7 +21,7 @@ app.use(express.static(distPath));
 
 //GET
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.resolve('./public/index.html'));
+  res.status(200).sendFile(path.resolve('./dist/index.html'));
 });
 
 
