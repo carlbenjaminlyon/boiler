aa663c7713fcf02fafa144b6e18f1c9afe1da69d
Ben Lyon <carlbenjaminlyon@gmail.com>
2021-11-28 11:52:59 -0600

    added restaurant storage functionality

--- boiler/src/client/components/Search.jsx
+++ boiler/src/client/components/Search.jsx
@@ -8,28 +8,36 @@ const key = require('../../../config/keys').yelp.APIkey;
 const Search = () => {
 
   const [ store, useStore ] = useState([]);
-  
+  console.log('store', store[0]);
   const getCrawfish = () => {
-    return axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=neworleans&categories=crawfish`, {
+    return axios.get('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=neworleans&categories=crawfish', {
       headers: {
-        Authorization: `Bearer ${key}`
+        Authorization: `Bearer ${key}`,
+        Origin: '',
+        'X-Requested-With': ''
       }
     })
-    .then((response) => response.data.businesses)
-    .then((businesses) => useStore(businesses))
-    .catch(err => console.error('error in yelp api call: ', err));
+      .then((response) => response.data.businesses)
+      .then((businesses) => { useStore(businesses)
+        businesses.forEach((store) => {
+          axios.post('/api/restaurants', {title: store.name, price: store.price, address: store.location.address1, lat: store.coordinates.latitude, long: store.coordinates.longitude})
+            .then((res) => { console.log('Saved Restaurant' ); })
+            .catch((err) => { console.log('Unable to save restaurant'); });
+        });
+      })
+      .catch(err => console.error('error in yelp api call: ', err));
   };
-  
 
-// useEffect(() => {
-//   useStore
-// })
-  
-  
+
+  // useEffect(() => {
+  //   useStore
+  // })
+
+
   return (
     <div>
       <button onClick={getCrawfish}>where da crawfish</button>
-      <p>{`Your Stores:`}</p>
+      <p>{'Your Stores:'}</p>
       <div>
         {
           store.map((x) => (
@@ -38,7 +46,7 @@ const Search = () => {
         }
       </div>
     </div>
-  )
+  );
 };
 
 export default Search;
